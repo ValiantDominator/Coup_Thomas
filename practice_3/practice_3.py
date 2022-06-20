3a21e64e58b195f5d8f3cabc65b378c2f87fdd70
ValiantDominator <thomasc3h8@gmail.com>
2022-06-20 17:45:27 -0500

    practice problem 3

---
+++ practice_3/practice_3.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 14 09:39:49 2022
+
+@author: Thomas
+"""
+
+"""
+Objectives:
+    - Interactions between classes
+    - Defensive programming
+    - Write to a coup file
+"""
+
+"""
+Instructions:
+    - Copy this whole file into your own directory
+    - Remove the instructor's name from the top and place your name
+        - do the same with creation date
+    - Follow the problem specifications that follow and make a python
+      document that can run without any errors
+    - For a hope score, push your homework to github by Sunday at 8:00 PM
+        - If you submit after that, it's fine, but you get 0 marks if
+          I grade and don't find your homework
+"""
+
+
+"""
+Michael, Philip, and Thomas can skip problem 1 if they wish
+"""
+# Problem 1
+# Write a function that takes a string as an argument
+# Returns: the longest substring in that string after splitting the string
+#          based on spaces.
+# Example: my_func("hello world my name is Daniel")
+#          returns: "Daniel"
+def longestWord (sentence):
+    if type(sentence) is str:
+        wordss = sentence.split(" ")
+        wordss.sort(key=len)
+        return wordss[-1]
+    else:
+        print("Error; enter a string")
+        return None
+print(longestWord("hello world my name is Daniel"))
+# Problem 2
+# In any way you see fit, find a way to *programmatically* determine whether
+# the 8 files `game_*.coup` are valid games.
+# You probably won't directly use this in your coup Game_Master,
+# but you'll definitely implement a better game master with this knowledge.
+# Recommendation: write some helper functions that are modular and easily
+#                 usable in your coup.py file you'll make later on.
+def coupResultParser (name_of_coup_file):
+    file.open
+
+def gameResult (name_of_coup_file):
+    gametext = coupResultParser(name_of_coup_file)
+
+# Problem 3
+# Build yourself a 'coup' directory and make a file called coup.py there.
+# Build a player class in coup.py
+    # Data
+        # Coins
+        # Cards
+        # log ### Whenever the game writes to its game file, it also writes
+              ### to each player's log
+    # act()
+        # If you have fewer than 7 coins, tax. Else, coup another player
+        # You might have to parse the log to find another active player's name.
+
+
+# Problem 4
+# Copy over your deck class from practice 2 to coup.py
+# Add the draw() method, which returns the first (top) card in the deck
+# and then removes that card from the deck
+
+# Problem 5
+# Build a class Game_Master in coup.py
+    # Data:
+        # players
+        # gamefile (file opened by game_init() and closed when the game ends)
+        # deck
+    # Methods:
+        # game_init() (Note, this is *not* the __init__ function)
+            # Arguments: the names of the players
+            # Initializes the deck with 5 cards (they can all be dukes for now)
+            # Give each player 2 cards and 2 coins
+        # turn()
+            # Process one action, such as "a tax" or "markus steal daniel"
+            # Check to see whether that action was valid
+            # Write that action to a gamefile and each player's log
+            # If a player had to discard, also write that action to a gamefile
+            # Update player cards and coins accordingly
+        # game()
+            # initialize a game and call turn() until the game is over
+# Play a FFA game with 3 players and submit the file in your practice_3
+# directory (since your Game_Master class lives in another directory)
+
+"""
+The following problem is for Michael, Thomas, and Philip
+"""
+# Problem 6
+# Assume your player class only plays 1v1 coup.
+# What is the optimal strategy for a no blocking, no challenging coup?
+# Adjust your player class to adopt this strategy.
+# To receive 4/4 on this assignment, I must *not* be able to design
+# An agent to beat your player (no matter who goes first)
---
+++ practice_3/week_3_demo.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 14 09:38:45 2022
+
+@author: dsmishler
+"""
+
+# Preliminaries
+    # Push to github! Otherwise I can't even see what you've tried
+    # It's good practice to push your code to github every time you stop coding
+        # Even if you're not done yet!
+    # Practice 2
+        # I notice that I'm the author of a lot of your code,
+        # please change that so you can preserve a shareable deliverable
+    # Don't know coup?
+        # https://youtu.be/a8bY3zI9FL4
+        # https://youtu.be/lPlBDZnxHQA
+# Go to https://github.com/DSMishler/Coup_Instructor and follow along
+
+# We made it here. Here's what we know:
+    # variables
+    # conditionals
+    # files
+    # lists
+    # functions
+    # classes
+# How do we build coup?
+    # Defensive programming
+
+
+
+
+
+"""
+It is crucial that you follow the conventions of the game file so that your
+coup agents can play with other hosts
+"""
+
+
+
+
+
+
+
+
+
+# What classes will we have?
+    # Deck
+        # cards
+        # draw()
+        # shuffle()
+        # add() (adds to the top of deck)
+    # Player
+        # cards
+        # coins
+        # act()
+        # react() ### Not needed now
+    # Game_Master
+        # players
+        # gamefile
+        # game()
+        # turn()
+        # check_legal()
+# Note: adding more methods, espeically for printing, is a *good* idea
